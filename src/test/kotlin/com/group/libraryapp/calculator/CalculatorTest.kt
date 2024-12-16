8649441466362ab382f18d53d21c5f06c42b746b
hobit22 <hobit22@naver.com>
2024-12-16 20:56:50 +0900

    [ADD] POJO calculator test

--- src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
+++ src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -4,16 +4,49 @@ fun main() {
     val calculatorTest = CalculatorTest()
 
     calculatorTest.addTest()
+    calculatorTest.minusTest()
+    calculatorTest.multiplyTest()
 }
 
 class CalculatorTest {
 
     fun addTest() {
+        // given
         val calculator = Calculator(5)
         calculator.add(3)
 
+        // when
         val expectedCalculator = Calculator(8)
 
+        // then
+        if (calculator != expectedCalculator) {
+            throw IllegalStateException()
+        }
+    }
+
+    fun minusTest() {
+        // given
+        val calculator = Calculator(5)
+        calculator.minus(3)
+
+        // when
+        val expectedCalculator = Calculator(2)
+
+        // then
+        if (calculator != expectedCalculator) {
+            throw IllegalStateException()
+        }
+    }
+
+    fun multiplyTest() {
+        // given
+        val calculator = Calculator(5)
+        calculator.multiply(3)
+
+        // when
+        val expectedCalculator = Calculator(15)
+
+        // then
         if (calculator != expectedCalculator) {
             throw IllegalStateException()
         }
