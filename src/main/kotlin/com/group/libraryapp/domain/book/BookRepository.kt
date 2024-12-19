b2ca7e77432e10e5a0de4fb2fd61a9840a6f33cf
hobit22 <hobit22@naver.com>
2024-12-19 23:38:35 +0900

    [REF] from java to kotlin repository 리펙토링

---
+++ src/main/kotlin/com/group/libraryapp/domain/book/BookRepository.kt
@@ -0,0 +1,9 @@
+package com.group.libraryapp.domain.book
+
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
+
+interface BookRepository : JpaRepository<Book, Long> {
+
+    fun findByName(bookName: String): Optional<Book> // ? 를 통해 Optional 대체 가능
+}
--- src/main/kotlin/com/group/libraryapp/domain/user/User.kt
+++ src/main/kotlin/com/group/libraryapp/domain/user/User.kt
@@ -1,7 +1,7 @@
 package com.group.libraryapp.domain.user
 
 import com.group.libraryapp.domain.book.Book
-import com.group.libraryapp.domain.user.loanHistory.UserLoanHistory
+import com.group.libraryapp.domain.user.loanhistory.UserLoanHistory
 import javax.persistence.*
 import javax.persistence.GenerationType.IDENTITY
 
