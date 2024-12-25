6fd00acbe32fb0c470cf6ee6558dbab5bb9f0c45
hobit22 <hobit22@naver.com>
2024-12-26 00:46:56 +0900

    [REF] 서버에서 db로 로직 리펙토링

--- src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepository.kt
+++ src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepository.kt
@@ -5,5 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository
 interface UserLoanHistoryRepository : JpaRepository<UserLoanHistory, Long> {
 
     fun findByBookNameAndStatus(bookName: String, status: UserLoanStatus): UserLoanHistory?
+
     fun findAllByStatus(loaned: UserLoanStatus): List<UserLoanHistory>
+
+    fun countByStatus(loaned: UserLoanStatus): Long
 }
