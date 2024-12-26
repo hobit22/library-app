a6752b682e8998b90374a6b20e43b76b12b88f3e
hobit22 <hobit22@naver.com>
2024-12-26 10:56:17 +0900

    [FIX] getStats querydsl 로 수정

--- src/main/kotlin/com/group/libraryapp/domain/book/BookRepository.kt
+++ src/main/kotlin/com/group/libraryapp/domain/book/BookRepository.kt
@@ -8,7 +8,7 @@ interface BookRepository : JpaRepository<Book, Long> {
 
     fun findByName(bookName: String): Book? // ? 를 통해 Optional 대체 가능
 
-    @Query("SELECT NEW com.group.libraryapp.dto.book.response.BookStatResponse(b.type, COUNT(b.id)) " +
-            "FROM Book b GROUP BY b.type")
-    fun getStats(): List<BookStatResponse>
+//    @Query("SELECT NEW com.group.libraryapp.dto.book.response.BookStatResponse(b.type, COUNT(b.id)) " +
+//            "FROM Book b GROUP BY b.type")
+//    fun getStats(): List<BookStatResponse>
 }
